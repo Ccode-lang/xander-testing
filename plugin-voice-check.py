44004cd3a262bd8fb5bd37d37fdec1c87e17fad7
Ccode-lang <78437178+Ccode-lang@users.noreply.github.com>
2023-10-21 18:34:12 -0700

    Create plugin-voice-check.py

---
+++ plugin-voice-check.py
@@ -0,0 +1,17 @@
+from xander_plugin import *
+
+
+def onload():
+    log("Voice plugin loaded!")
+
+async def onmessage(message):
+    if message.content.startswith("!join "):
+        await join_vc(message.author.voice.channel)
+        return False
+    elif message.content == ("!leave"):
+        await leave_vc()
+    return True
+
+
+def onexit():
+    log("Voice plugin exit run.")
